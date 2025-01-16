de4bb8929977d86522735807876e092836729ce4
waleedcj <uwonttakemealive10@gmail.com>
2025-01-16 23:20:33 +0400

    remove footer border

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import { FaHeart } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="w-full border-t border-border/40">
+    <footer className="w-full">
       <div className="container flex items-center justify-center py-11">
         <p className="text-sm text-muted-foreground flex items-center gap-1">
           Made with <FaHeart className="w-4 h-4 text-red-500 fill-red-500" /> by{" "}
