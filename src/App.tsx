3e414884db96fae8487002f3692a16c012586012
waleedcj <uwonttakemealive10@gmail.com>
2025-02-15 05:34:35 +0400

    add GA

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,16 @@ import SkillsSection from "./components/skills-section";
 import { BrowserRouter } from "react-router-dom";
 import { Header } from "./components/Header";
 import Footer from "./components/footer";
+import { useEffect } from "react";
+import { initGA } from "./utils/analytics";
 
 /* <div class="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div> 
 <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>*/
 
 function App() {
+  useEffect(() => {
+    initGA();
+  }, []);
   return (
     <ThemeProvider>
       <BrowserRouter>
