dc13a8cdcb212f0293e42a4916c97ff0c758073c
waleedcj <uwonttakemealive10@gmail.com>
2025-02-15 19:38:22 +0400

    remove extra settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
+// https://vite.dev/config/
+export default defineConfig({
   plugins: [react()],
-  define: {
-    __VITE_TRACKING_ID__: JSON.stringify(env.VITE_TRACKING_ID),
-  },
-}
 })
